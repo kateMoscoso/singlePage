81366bfd6a5a7de91451ba8b147ea327533224a7
kateMoscoso <katiemscs@gmail.com>
2019-03-06 21:40:44 +0100

    pug and stylus

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 export default {
-    appname: 'platzimusic',
-    apiKey: '',
-    secret: '',
-    registeredTo: ''
+    appname: 'vueMusic',
+    apiKey: '55deda1fb1980f2d669589c9528187fb',
+    secret: 'b867a5a25c42f98c24887e7b11f9ec8f',
+    registeredTo: 'Kate_Sp'
 }
